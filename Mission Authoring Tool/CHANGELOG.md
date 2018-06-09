c8e12c2358c009b84796473d283ec00a5c790575
lucka-me <samzhao0727@gmail.com>
2018-06-09 20:38:37 +0800

    Update Mission Authoring Tool [0.1.1] Finished - Waypoint add, remove and clear all
    Changed
    - Interface improved

--- Mission Authoring Tool/CHANGELOG.md
+++ Mission Authoring Tool/CHANGELOG.md
@@ -3,6 +3,15 @@
 <h1 align=center><br/>更新日志</h1>
 
 ```markdown
+## [0.1.1] - 2018-06-09
+### Finished
+- Waypoint add, remove and clear all
+
+### Changed
+- Interface improved
+```
+
+```markdown
 ## [0.1.0] - 2018-06-09
 - Initial version, a prototype without function
 ```
--- Mission Authoring Tool/lib/main.js
+++ Mission Authoring Tool/lib/main.js
@@ -21,18 +21,46 @@ function initMap() {
         map.addControl(new AMap.Scale());
         map.addControl(new AMap.MapType());
     });
+    map.on('click', function(event) {
+        createWaypoint(event.lnglat);
+    })
 }
 
-function removeAt(i) {
-    waypointList.split(i, 1);
-    map.remove(markerList[i]);
-    markerList.split(i, 1);
+function createWaypoint(lnglat) {
+    var newMarker = new AMap.Marker({
+        position: lnglat,
+        label: {content: String(waypointList.length + 1), offset: AMap.Pixel(0, 0)}
+    });
+    markerList.push(newMarker);
+    map.add(newMarker);
+
+    waypointList.push({lnglat});
+
+    var newCard = document.querySelector("#template-card");
+    newCard.content.querySelector(".waypoint-card").setAttribute("id", "waypointCard" + String(waypointList.length - 1));
+    newCard.content.querySelector("#waypointIndex").innerHTML = String(waypointList.length);
+    newCard.content.querySelector("#removeButton").setAttribute("onclick", "removeAt(" + String(waypointList.length - 1) + ")");
+    document.getElementById("waypointList").appendChild(newCard.content.cloneNode(true));
+}
+
+function removeAt(index) {
+    waypointList.splice(index, 1);
+    map.remove(markerList[index]);
+    markerList.splice(index, 1);
+    for (var i = index; i < markerList.length; i++) {
+        markerList[i].setLabel({content: String(i + 1), offset: AMap.Pixel(0, 0)});
+    }
+    var card = document.getElementById("waypointCard" + String(index));
+    card.parentNode.removeChild(card);
+    for (var i = index + 1; i < markerList.length + 1; i++) {
+        var card = document.getElementById("waypointCard" + String(i));
+        card.setAttribute("id", "waypointCard" + String(i - 1));
+        card.querySelector("#waypointIndex").innerHTML = String(i);
+    }
 }
 
 function clearWaypoints() {
-    waypointList = [];
-    for (var i = 0; i < markerList.length; i++) {
-        map.remove(markerList[i]);
+    while (waypointList.length > 0) {
+        removeAt(0);
     }
-    markerList = [];
 }
